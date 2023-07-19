af20555cb56dd42cc800be3846a2af9ff59016c9
Michelle Lamelza <mishlamelza@gmail.com>
2023-07-19 06:43:48 -0400

    deleted BrowserRouter from App.js

--- product-listing/src/App.js
+++ product-listing/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
+//import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import NavBar from './Components/NavBar';
+
 
 function App() {
   return (
     <div className="App">
+     
+        <NavBar/>
+        
 
     </div>
   );
